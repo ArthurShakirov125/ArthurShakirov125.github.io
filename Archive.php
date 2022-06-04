042cef20d849803be27da5049fe78326d3d12af6
Shakirov_Arthur <Wolverane32@gmail.com>
2022-06-04 21:57:05 +0500

    bb

---
+++ Archive.php
@@ -0,0 +1,37 @@
+<?php
+
+
+$rootPath = realpath('images');
+
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootPath),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+
+$zip = new ZipArchive(); //Создаём объект для работы с ZIP-архивами
+$zip->open("archive.zip", ZIPARCHIVE::CREATE); //Открываем (создаём) архив archive.zip
+
+foreach ($files as $name => $file)
+{
+    // Skip directories (they would be added automatically)
+    if (!$file->isDir())
+    {
+        // Get real and relative path for current file
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        // Add current file to archive
+        $zip->addFile($filePath, $relativePath);
+    }
+}
+
+
+$zip->addFile("index.php"); //Добавляем в архив файл index.php
+$zip->addFile("styles/main.css"); //Добавляем в архив файл styles/style.css
+$zip->addFile("styles/styleLess.css"); //Добавляем в архив файл styles/style.css
+$zip->close(); //Завершаем работу с архивом
+?>
+
+<div>
+    <a href="archive.zip" download>скачать</a>
+</div>
---
+++ app.js
@@ -0,0 +1,3 @@
+"use strict";
+"use strict";
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudDEuanMiLCJtYWluLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FDQUEiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOltudWxsLCIiXX0=
